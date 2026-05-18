9dbb5bf35b79ed71f826a9eb53088efac9e18c6b
shimoawazu <n_hiroshige@mac.com>
2026-05-18 11:07:13 +0900

    debug: set DEBUG_ASSET_URL to AdobeStock image for problem 1 isolation

--- blocks/ad-creation/ad-creation.js
+++ blocks/ad-creation/ad-creation.js
@@ -11,7 +11,7 @@ const POLL_INTERVAL_MS = 3000;
 // 【デバッグ用 — 問題1の切り分け】空でない場合はフォーム入力より優先される
 // 実際のAEM DAM画像URLを設定して動作確認後 '' に戻す
 // 例: 'https://author-p154442-e1620921.adobeaemcloud.com/content/dam/referencedemo-275/image.jpg'
-const DEBUG_ASSET_URL = '';
+const DEBUG_ASSET_URL = 'https://author-p154442-e1620921.adobeaemcloud.com/content/dam/demo/Project%20H/project-h2/AdobeStock_192386403.jpeg';
 
 const CONNECTIONS = [
   { connectionId: 'xy-edge__node_1773092259_4401688f_outputs-node_1773092259_d8b8daa5_input-images', connectionSource: 'node_1773092259_4401688f', connectionTarget: 'node_1773092259_d8b8daa5', sourcePort: 'outputs', targetPort: 'input-images' },
